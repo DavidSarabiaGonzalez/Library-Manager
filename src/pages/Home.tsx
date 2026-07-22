4b3267ab362049f2535e885176fc7243898d756a
David Sarabia <davidsarabiagonzalez06@gmail.com>
2026-07-22 06:16:06 +0200

    Agregar componente Home con título y login

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Login } from "../components/Login";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Aplicacion para gestion de bibliotecas</h2>
+      <Login></Login>
+    </>
+  );
+}
