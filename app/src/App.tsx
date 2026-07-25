6afe7eb277ad0b0df6ee2762038fd7d51644e72b
David Sarabia <davidsarabiagonzalez06@gmail.com>
2026-07-25 18:01:45 +0200

    feat: agregar componente DashBoard y mejorar estructura de Home

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,17 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import AuthCallback from './pages/AuthCallback'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AuthCallback from "./pages/AuthCallback";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/auth/callback' element={<AuthCallback />}></Route>
-      
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/auth/callback" element={<AuthCallback />}></Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -3,8 +3,10 @@ import { Login } from "../components/Login";
 export default function Home() {
   return (
     <>
-      <h2>Aplicacion para gestion de bibliotecas</h2>
-      <Login></Login>
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="">Aplicacion para gestion de bibliotecas</h2>
+        <Login></Login>
+      </div>
     </>
   );
 }
