37cd0d8a7a0f160f91951c0179bee9abec320516
David Sarabia <davidsarabiagonzalez06@gmail.com>
2026-07-31 05:47:45 +0200

    feat: eliminar clases de desbordamiento en el componente Home para mejorar el diseño

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -5,10 +5,10 @@ import { Outlet } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <div className="zones-container min-w-full overflow-hidden min-h-screen bg-gray-200 ">
+      <div className="zones-container min-w-full min-h-screen bg-gray-200 ">
         <Header />
         <SideBar />
-        <main className="[grid-area:main] m-4 flex flex-col gap-6 justify-start items-center overflow-hidden">
+        <main className="[grid-area:main] m-4 flex flex-col gap-6 justify-start items-center">
           <Outlet />
         </main>
       </div>
