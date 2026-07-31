82f6932c8693f906bfd42771e494a7449d0cb8b8
David Sarabia <davidsarabiagonzalez06@gmail.com>
2026-07-31 05:47:38 +0200

    feat: mejorar el componente Header con navegación dinámica y ajustes de estilo

--- app/src/sections/Dashboard.tsx
+++ app/src/sections/Dashboard.tsx
@@ -1,19 +1,79 @@
+import { useEffect, useState } from "react";
+import { BookPreview } from "../components/BookPreview";
+import { ChartComponent } from "../components/ChartComponent";
 import InfoCard from "../components/InfoCard";
+import { getViewDashboard } from "../lib/views";
 
 export default function DashBoard() {
+  interface DashboardStats {
+    totalBooks: number;
+    availableBooks: number;
+    totalUsers: number;
+    activeLoans: number;
+  }
+
+  const [stats, setStats] = useState<DashboardStats>({
+    totalBooks: 0,
+    availableBooks: 0,
+    totalUsers: 0,
+    activeLoans: 0,
+  });
+
+  useEffect(() => {
+    async function load() {
+      const data = await getViewDashboard();
+
+      setStats({
+        totalBooks: data.total_books,
+        availableBooks: data.available_books,
+        totalUsers: data.total_users,
+        activeLoans: data.active_loans,
+      });
+    }
+
+    load();
+  }, []);
+
+  const loansData = [
+    { time: "2026-07-01", value: 12 },
+    { time: "2026-07-02", value: 18 },
+    { time: "2026-07-03", value: 15 },
+    { time: "2026-07-04", value: 23 },
+    { time: "2026-07-05", value: 20 },
+    { time: "2026-07-06", value: 28 },
+  ];
+  console.log(stats);
   return (
     <>
       <section className="grid grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        <InfoCard text="Total de libros" quantity="12.4K" to="/books" />
-        <InfoCard text="Disponible" quantity="8.9K" to="/books" />
-        <InfoCard text="Prestamos activos" quantity="789" to="/loans" />
-        <InfoCard text="Usuarios registrados" quantity="3.2K" to="/users" />
+        <InfoCard
+          text="Total de libros"
+          quantity={String(stats.totalBooks)}
+          to="/books"
+        />
+        <InfoCard
+          text="Disponible"
+          quantity={String(stats.availableBooks)}
+          to="/books"
+        />
+        <InfoCard
+          text="Prestamos activos"
+          quantity={String(stats.activeLoans)}
+          to="/loans"
+        />
+        <InfoCard
+          text="Usuarios registrados"
+          quantity={String(stats.totalUsers)}
+          to="/users"
+        />
       </section>
 
-      <section className=" grid grid-cols-5 justify-center items-center w-full h-1/2 gap-6">
-        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-4 col-span-3 rounded-md flex flex-col justify-between">
+      <section className=" grid grid-cols-1 lg:grid-cols-5 justify-center items-center w-full h-auto gap-6">
+        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-4 col-span-3 rounded-md flex flex-col justify-between gap-4">
           <div className="flex flex-row justify-between items-center">
-            <h4>Prestamos evolucion</h4>
+            <h2 className="text-sm font-bold sm:text-base lg:text-lg md:text-3xl">
+              Prestamos evolucion
+            </h2>
 
             <select
               name="graphic"
@@ -26,21 +86,42 @@ export default function DashBoard() {
             </select>
           </div>
 
-          <canvas className="max-w-20"></canvas>
+          <ChartComponent data={loansData} />
         </article>
 
-        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-2 rounded-md col-span-2">
-          <h4>Popular</h4>
+        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-4 rounded-md col-span-2 flex flex-col justify-start items-start">
+          <h2 className="text-sm font-bold sm:text-bqase lg:text-lg md:text-3xl">
+            Popular
+          </h2>
+          <div className="pt-2 flex flex-col gap-4">
+            <BookPreview
+              title="Aprendiento Git y GitHub"
+              author={["Miguel Angel Duran"]}
+              category={["Educativo"]}
+              available="AVAILABLE"
+            />
+
+            <BookPreview
+              title="Harry Potter"
+              author={["J. K. Rowling"]}
+              category={["Magico", "Sobrenatural"]}
+              available="DAMAGED"
+            />
+          </div>
         </article>
       </section>
 
-      <section className=" grid grid-cols-5 justify-center items-center w-full h-1/2 gap-6">
-        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-2  col-span-3 rounded-md">
-          <h4>Prestamos evolucion</h4>
+      <section className=" grid lg:grid-cols-5 grid-cols-1 justify-center items-center w-full gap-6">
+        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-4  col-span-3 rounded-md">
+          <h2 className="text-sm font-bold sm:text-base lg:text-lg md:text-3xl">
+            Actividad reciente
+          </h2>
         </article>
 
-        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-2 rounded-md col-span-2">
-          <h4>Popular</h4>
+        <article className="border border-gray-300 aspect-auto w-full h-full box-border p-4 rounded-md col-span-2">
+          <h2 className="text-sm font-bold sm:text-base lg:text-lg md:text-3xl">
+            Préstamos populares
+          </h2>
         </article>
       </section>
     </>
--- app/src/sections/Header.tsx
+++ app/src/sections/Header.tsx
@@ -1,29 +1,47 @@
-import { BellIcon, LoupeIcon } from "../assets/icons/icons";
+import { BellIcon, LoupeIcon } from "../assets/icons/Icons";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+
+  const titles: Record<string, string> = {
+    "/": "Dashboard",
+    "/books": "Libros",
+    "/loans": "Prestamos",
+    "/authors": "Autores",
+    "/users": "Usuarios",
+  };
+
+  const title = titles[location.pathname] ?? "DashBoard";
   return (
     <>
-      <header className="[grid-area:header] flex flex-row justify-between items-center border-b-2 px-8 border-gray-300 w-full h-[8vh] top-0 box-border">
-        <form
-          role="search"
-          className="flex items-center w-full max-w-xl bg-white border border-gray-300 rounded-lg px-3 py-2 focus-within:border-blue-500 transition"
-        >
-          <LoupeIcon className="size-5 text-gray-400 mr-2" />
+      <header className="[grid-area:header] flex flex-row justify-center md:justify-around items-center border-b-2 px-8 border-gray-300 w-full h-[8vh] top-0">
+        <h1 className="text-2xl font-bold md:hidden flex flex-row justify-center items-center">
+          {title}
+        </h1>
 
-          <input
-            type="search"
-            placeholder="Introduce un libro, autor, usuario..."
-            className="w-full outline-none bg-transparent text-sm"
-          />
-        </form>
+        <div className="hidden md:flex w-full flex-row justify-between items-center">
+          <form
+            role="search"
+            className="flex items-center w-full max-w-sm lg:max-w-xl bg-white border border-gray-300 rounded-lg px-3 py-2 focus-within:border-blue-500 transition box-border"
+          >
+            <LoupeIcon className="size-5 text-gray-400 mr-2" />
 
-        <div className="flex flex-row gap-4 justify-center items-center">
-          <BellIcon className="size-8 text-gray-700 cursor-pointer  " />
-          <img
-            src="src/assets/Placeholder_Avatar.webp"
-            alt=""
-            className="size-12 rounded-full cursor-pointer"
-          />
+            <input
+              type="search"
+              placeholder="Introduce un libro, autor, usuario..."
+              className="w-full outline-none bg-transparent text-sm"
+            />
+          </form>
+
+          <div className="flex flex-row gap-4 justify-center items-center">
+            <BellIcon className="size-8 shrink-0 text-gray-700 cursor-pointer  " />
+            <img
+              src="src/assets/Placeholder_Avatar.webp"
+              alt=""
+              className="size-12 rounded-full cursor-pointer"
+            />
+          </div>
         </div>
       </header>
     </>
